48f5fcc64b090c8a139a09d79f114220308eb033
Isidora Stanisavljevska <137770804+moonrose93@users.noreply.github.com>
2024-04-29 09:58:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,11 +23,3 @@ document.addEventListener('click',(event)=>{
 })
 
 
-/*Here we added event listener to the document itself and we say if the event on click dont target the hambureger menu the other elements from the menu then the menu
-to be displayed none.
-So basically we have to include the hamburger icon itself so that the event listener of the hamburger icon to be triggered.
-
--Ako eventot na klik ne gi targetira ovie mesta togas menito da bide sokrieno.
-
-Na samiot dokument stavame event listener i ako toj event listener na clik ne gi targetira posakuvanite elementi togas menito ke se se sokrie ako eventot
-gi targetira posakuvanite elementi togas menito ke si ostane.*/
